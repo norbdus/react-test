f523a93c5b98091f364a3cbf6afe90bbba0cf237
Vitório <norbdus@gmail.com>
2018-10-21 22:30:27 -0300

    add postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_21_135637) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "description"
     t.boolean "done", default: false
