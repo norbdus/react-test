a3364fdae0df948e8fe0e0ac10eb7c3bdbbec00a
Vitório <norbdus@gmail.com>
2018-10-21 23:21:28 -0300

    ajuste css

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -7,8 +7,15 @@ const NewItem = (props) => {
             props.handleFormSubmit(formFields.description.value); e.target.reset();
         }
         } >
-            <input ref={input => formFields.description = input} placeholder='Descreva a Task' />
-            <button className="waves-effect waves-light btn" >Criar</button>
+            <div className="row">
+                <div className="input-field col s12">
+                    <div className='col s10'>
+                        <i className="material-icons prefix">content_paste</i>
+                        <input ref={input => formFields.description = input} placeholder='Descreva a Task' />
+                    </div>
+                    <button className="waves-effect waves-light btn col s2" >Criar</button>
+                </div>
+            </div>
         </form>
     )
 }
